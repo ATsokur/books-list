99a880fba9b8be73d5d907aebba122fe4ce50877
ATsokur <flamer721@gmail.com>
2025-11-07 21:29:43 +0300

    refactor: delete nx welcome

--- apps/books-list/src/app/app.component.spec.ts
+++ apps/books-list/src/app/app.component.spec.ts
@@ -1,11 +1,10 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { NxWelcome } from './nx-welcome';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, NxWelcome],
+      imports: [AppComponent]
     }).compileComponents();
   });
 
