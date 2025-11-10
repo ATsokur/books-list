71786c0fa2328aa67b72b3c078d8928b260a060d
ATsokur <flamer721@gmail.com>
2025-11-10 09:08:00 +0300

    fix: book-add-form

--- libs/books/src/lib/feature-books-list/book-add-form/book-add-form.component.ts
+++ libs/books/src/lib/feature-books-list/book-add-form/book-add-form.component.ts
@@ -1,41 +1,25 @@
-import { AsyncPipe } from '@angular/common';
 import { Component, output } from '@angular/core';
-import {
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators
-} from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { TuiAppearance, TuiError, TuiTextfield } from '@taiga-ui/core';
-import { TuiFieldErrorPipe, tuiValidationErrorsProvider } from '@taiga-ui/kit';
+import { TuiAppearance, TuiTextfield } from '@taiga-ui/core';
 import { TuiTextfieldControllerModule } from '@taiga-ui/legacy';
 import { BookAddForm } from '@bl/data-access';
 
 @Component({
   selector: 'lib-book-add-form',
   imports: [
-    AsyncPipe,
     TuiTextfield,
     TuiAppearance,
-    TuiError,
-    TuiFieldErrorPipe,
     ReactiveFormsModule,
     TuiTextfieldControllerModule
   ],
   templateUrl: './book-add-form.component.html',
   styleUrl: './book-add-form.component.scss',
-  providers: [
-    tuiValidationErrorsProvider({
-      required: 'Поле не может быть пустым!'
-    })
-  ]
+  providers: []
 })
 export class BookAddFormComponent {
   protected readonly form = new FormGroup<BookAddForm>({
-    title: new FormControl<string | null>(null, {
-      validators: [Validators.required]
-    })
+    title: new FormControl<string | null>(null)
   });
 
   readonly bookTitle = output<string>();
@@ -51,15 +35,11 @@ export class BookAddFormComponent {
   }
 
   protected onAdd() {
-    this.form.markAllAsTouched();
-    this.form.updateValueAndValidity();
-
     const title = this.form.controls.title.getRawValue();
 
-    if (this.form.invalid || !title) return;
+    if (!title) return;
 
     this.bookTitle.emit(title);
     this.form.controls.title.patchValue(null, { emitEvent: false });
-    this.form.controls.title.markAsUntouched({ emitEvent: false });
   }
 }
